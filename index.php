896c46b7cc82670d17b4af788a73a93a0a67e636
f95f <95felipefernandes@gmail.com>
2024-09-21 22:36:46 -0300

    feat: adiciona estrutura do  menu lateral.

--- index.php
+++ index.php
@@ -10,7 +10,36 @@
 </head>
 
 <body>
-    
+    <div class="side-menu">
+        <div class="side-menu-header">
+            <div class="logo">
+                <img src="../images/logo.svg" alt="logo: journalling">
+                <span>Journalling</span>
+            </div>
+            <hr class="light-separator">
+        </div>
+        <div class="side-menu-body">
+            <ul>
+                <li class="menu-item">
+                    <i class="fa-solid fa-diagram-project"></i>
+                    Projetos
+                </li>
+                <li class="menu-item active">
+                    <i class="fa-solid fa-list-check"></i>
+                    Tarefas
+                </li>
+                <li class="menu-item">
+                    <i class="fa-regular fa-user"></i>
+                    Usuários
+                </li>
+            </ul>
+        </div>
+        <div class="side-menu-footer">
+            <hr class="light-separator">
+            <span>© Projeto LDPD5</span>
+            <span>2024</span>             
+        </div>
+    </div>
         <?php include('pages/tasklist.php'); ?>
 </body>
 
