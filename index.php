747bd9e8e99daebe822c47c1fdde60b2818ab74a
f95f <95felipefernandes@gmail.com>
2024-09-24 21:53:37 -0300

    fix: colocando página correta no roteamento.

--- index.php
+++ index.php
@@ -52,8 +52,8 @@
     </div>
 
     <div class="content">
-        <?php //include('pages/tasklist.php'); ?>
-        <?php include('pages/projects.php'); ?>
+        <?php include('pages/tasklist.php'); ?>
+        <?php //include('pages/projects.php'); ?>
     </div>
 </body>
 
