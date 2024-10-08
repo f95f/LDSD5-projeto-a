05a578348e2728c616a21ad761a7451b85dfb2f2
f95f <95felipefernandes@gmail.com>
2024-10-08 01:28:32 -0300

    feat: adiciona roteamento.

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
+<?php
+    define("TITLE", "Início | Journalling");
+    define("PAGE", null);
+    define("STYLESHEET", null);
+?>
+
 <?php include('layout/side-menu.php'); ?>
 <?php include('layout/header.php'); ?>
     </div>
+
+    <script>
+        $(document).ready(function() {
+            var currentUrl = window.location.href; 
+            window.location.href = currentUrl + 'pages/inicio.php';
+        });
+    </script>
 </body>
 
 </html> 
--- pages/inicio.php
+++ pages/inicio.php
@@ -1,3 +1,12 @@
 <?php define("TITLE", "Início | Journalling"); ?>
+<?php define("PAGE", null); ?>
+<?php define("STYLESHEET", null); ?>
 <?php include __DIR__ . '/../layout/side-menu.php'; ?>
 <?php include __DIR__ . '/../layout/header.php'; ?>
+<header>
+    <div class="title">
+        <i class="fa-solid fa-house"></i>
+        <h1>Bem-vindo!</h1>
+    </div>
+</header>
+<main></main>
