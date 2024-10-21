4d8b8baa4612af88f6f27f8edc9823f861f6deba
vitoryur <161538894+vitoryur@users.noreply.github.com>
2024-10-20 21:38:01 -0300

    Ajuste calendario.php

--- pages/calendario.php
+++ pages/calendario.php
@@ -12,7 +12,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../styles/calendario.css">
     <script>
 
       document.addEventListener('DOMContentLoaded', function() {
@@ -37,7 +36,6 @@
 </header>
 <main>
             <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js'></script>
-
             <div id='calendar'></div>
 </main>
 <footer>
